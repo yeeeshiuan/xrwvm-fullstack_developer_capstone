dd55901343f145a01328374f539f25b048c46d89
Joey Huang <yeeeshiuan.huang@gmail.com>
2024-05-07 03:45:50 -0400

    add the car model

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,9 +11,10 @@ urlpatterns = [
     # path for login
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_request, name='logout'),
+    path(route='register', view=views.registration, name='register'),
 
     # path for dealer reviews view
-    path(route='register', view=views.registration, name='register'),
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 
     # path for add a review view
 
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,10 +30,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://yinxuanh-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://yinxuanh-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://yinxuanh-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+    'https://yinxuanh-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 
 REST_FRAMEWORK = {
